aff1eec7e185fa35bc2958451e507ed75066b048
thayari-alt <80105652+thayari-alt@users.noreply.github.com>
2021-06-14 02:01:20 +0300

    refactoring 15.10

--- script.js
+++ script.js
@@ -1,102 +1,126 @@
-let lastOperand = 0;
-let currentOperation = null;
-let currentOperand = null;
-let history = '';
-let result = null;
+class Calculator {
+  constructor() {
+    this.lastOperand = 0;
+    this.currentOperation = null;
+    this.currentOperand = null;
+    this.history = '';
+    this.result = null;
 
-const inputWindow = document.querySelector('#inputWindow');
-const btnClr = document.querySelector('#btn_clr');
-const btnResult = document.querySelector('#btn_result');
-const btnPlus = document.querySelector('#btn_plus');
-const btnMinus = document.querySelector('#btn_minus');
-const btnMult = document.querySelector('#btn_mult');
-const btnPoint = document.querySelector('#btn_point');
-const btnDiv = document.querySelector('#btn_div');
-const btnSqrt = document.querySelector('#btn_sqrt');
-const historyEl = document.querySelector('.history')
+    this.inputWindow = document.querySelector('#inputWindow');
+    this.btnClr = document.querySelector('#btn_clr');
+    this.btnResult = document.querySelector('#btn_result');
+    this.btnPlus = document.querySelector('#btn_plus');
+    this.btnMinus = document.querySelector('#btn_minus');
+    this.btnMult = document.querySelector('#btn_mult');
+    this.btnPoint = document.querySelector('#btn_point');
+    this.btnDiv = document.querySelector('#btn_div');
+    this.btnSqrt = document.querySelector('#btn_sqrt');
+    this.historyEl = document.querySelector('.history');
+  }
 
-for (let i = 0; i < 10; i++) {
-	document.querySelector(`#btn_${i}`).addEventListener('click', () => {
-		if (result) {
-			clear();
-		}
-		inputWindow.value += i
-	});
+  assignEventListeners() {
+    for (let i = 0; i < 10; i += 1) {
+      document.querySelector(`#btn_${i}`).addEventListener('click', () => {
+        if (this.result) {
+          this.clear();
+        }
+        this.inputWindow.value += i;
+      });
+    }
+
+    this.btnClr.addEventListener('click', this.clear.bind(this));
+
+    this.btnResult.addEventListener('click', () => this.calculate(this.currentOperation));
+
+    this.btnPoint.addEventListener('click', () => {
+      if (this.inputWindow.value.indexOf('.') === -1) {
+        this.inputWindow.value += '.';
+      }
+    });
+
+    this.operationHandler(this.btnPlus, this.btnMinus, this.btnMult, this.btnDiv, this.btnSqrt);
+  }
+
+  // совершает арифметическое действие в зависимости от операции
+  calculate(operation) {
+    if (operation !== '√') {
+      this.currentOperand = parseFloat(this.inputWindow.value);
+    }
+    switch (operation) {
+      case '+':
+        this.result = this.lastOperand + this.currentOperand;
+        break;
+      case '–':
+        this.result = this.lastOperand - this.currentOperand;
+        break;
+      case '×':
+        this.result = this.lastOperand * this.currentOperand;
+        break;
+      case '/':
+        this.result = this.lastOperand / this.currentOperand;
+        break;
+      case '√':
+        this.result = Math.sqrt(this.lastOperand);
+        break;
+      default:
+        break;
+    }
+    // добавить в историю второй операнд
+    this.history += ` ${this.currentOperand ? this.currentOperand : ''}`;
+    this.showHistory(this.history);
+    this.inputWindow.value = Number(this.result.toPrecision(12));
+  }
+
+  // выводит значение в поле истории
+  showHistory(value) {
+    this.historyEl.textContent = value;
+  }
+
+  // присваивает event listeners кнопкам операций
+  operationHandler(...args) {
+    args.forEach((element) => {
+      element.addEventListener('click', this.operationEvent.bind(this));
+    });
+  }
+
+  operationEvent(event) {
+    const operation = event.target.textContent;
+
+    // если минус набран первым перец цифрами
+    if (operation === '–' && this.inputWindow.value === '') {
+      this.inputWindow.value += '-';
+    } else if (this.inputWindow.value !== '' && !this.result) {
+      this.lastOperand = parseFloat(this.inputWindow.value);
+      this.history = `${this.inputWindow.value} ${operation}`;
+
+      if (operation === '√') { // корень из набранного числа - посчитать сразу
+        this.calculate(operation);
+      } else { // другая операция - добавить в историю
+        this.currentOperation = operation;
+        this.showHistory(this.history);
+        this.inputWindow.value = '';
+      }
+    }
+  }
+
+  clear() {
+    this.lastOperand = 0;
+    this.currentOperation = null;
+    this.currentOperand = null;
+    this.result = null;
+    this.inputWindow.value = '';
+    this.inputWindow.placeholder = 0;
+    this.history = '';
+    this.showHistory('');
+  }
+
+  init() {
+    this.clear();
+    this.assignEventListeners();
+  }
 }
 
-btnClr.addEventListener('click', clear);
-btnResult.addEventListener('click', () => calculate(currentOperation));
-
-btnPoint.addEventListener('click', () => {
-	if (inputWindow.value.indexOf('.') == -1) {
-		inputWindow.value += '.';
-	}
+document.addEventListener('DOMContentLoaded', () => {
+  const calculator = new Calculator();
+  calculator.init();
 });
-
-operationHandler(btnPlus, btnMinus, btnMult, btnDiv, btnSqrt);
-
-function calculate(operation) {
-	if (operation != '√') {
-		currentOperand = parseFloat(inputWindow.value);
-	}
-	switch (operation) {
-		case '+':
-			result = lastOperand + currentOperand;
-			break;
-		case '–':
-			result = lastOperand - currentOperand;
-			break;
-		case '×':
-			result = lastOperand * currentOperand;
-			break;
-		case '/':
-			result = lastOperand / currentOperand;
-			break;
-		case '√':
-			result = Math.sqrt(lastOperand);
-			break;
-		default:
-			break;
-	}
-	history += ` ${currentOperand ? currentOperand : ''}`;
-	showHistory(history);
-	inputWindow.value = Number(result.toPrecision(12));
-}
-
-function showHistory(value) {
-	historyEl.textContent = value;
-}
-
-function operationHandler() {
-	Array.from(arguments).forEach(element => {
-		element.addEventListener('click', (event) => {
-			let operation = event.target.textContent;
-			if (operation == '–' && inputWindow.value == '') {
-				inputWindow.value += '-';
-			} else if (operation == '√' && inputWindow.value != '' && !result) {
-				lastOperand = parseFloat(inputWindow.value);
-				history = inputWindow.value + ' ' + operation;
-				calculate(operation);
-			} else if (inputWindow.value != '' && !result) {
-				currentOperation = operation;
-				lastOperand = parseFloat(inputWindow.value);
-				history = inputWindow.value + ' ' + operation;
-				showHistory(history);
-				inputWindow.value = '';
-			}
-		});
-	});
-} 
-
-function clear() {
-	lastOperand = 0;
-	currentOperation = null;
-	currentOperand = null
-	result = null;
-	inputWindow.value = '';
-	inputWindow.placeholder = 0;
-	history = '';
-	showHistory('');
-};
-
-clear();
